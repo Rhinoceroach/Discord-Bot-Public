e7c2a8d94442b6c51fc5f29d4a4d9a8629fbca74
Rhinoceroach <whyamiso24.7@gmail.com>
2022-01-16 00:16:45 -0500

    port fix

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ const config = require("./Data/config.json");
 const Command = require("./Structures/Command.js")
 const client = new Client();
 
-const port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
-
 client.start(config.token);
 
 
